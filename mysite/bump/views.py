82730351996f09f42c808188035daa282d5aa2d9
Kaighn Kevlin <kaighn.kevlin@duke.edu>
2016-01-20 00:42:28 -0500

    added ranked row for finisher stats

--- mysite/bump/views.py
+++ mysite/bump/views.py
@@ -254,13 +254,17 @@ def player_profile(request, player_identifier):
     player = Player.objects.get(identifier__iexact=player_identifier)
     recent_games = Game.objects.filter(Q(winner__identifier__iexact = player_identifier) | Q(loser__identifier__iexact=player_identifier)).order_by('-date')[:20]
     user = player.user
+    ranked_players = [p for p in Player.objects.all() if p.ranked()]
     def div(a,b):
         if b==0:
             return 'NaN'
         else:
             return a/b
-    def get_finisher_stat(finisher,player):
-        return Game.objects.filter(finisher=finisher,winner=player).count()
+    def get_finisher_stat(finisher,player,ranked_opponents=False):
+        if ranked_opponents:
+            return Game.objects.filter(finisher=finisher,winner=player,loser__in=ranked_players).count()
+        else:
+            return Game.objects.filter(finisher=finisher,winner=player).count()
     def get_stats(games,player):
         whr = games.filter(advantage='hr',winner__in=player).count()
         wbr = games.filter(advantage='br',winner__in=player).count()
@@ -282,14 +286,14 @@ def player_profile(request, player_identifier):
         return win_totals,lose_totals,win_percentages,lose_percentages,player_stats
     player_games = Game.objects.filter(Q(winner__identifier__iexact = player_identifier) | Q(loser__identifier__iexact=player_identifier)).exclude(advantage='')
     wt,lt,wp,lp,ps = get_stats(player_games,[player])
-    ranked_players = [p for p in Player.objects.all() if p.ranked()]
     rwt,rlt,rwp,rlp,rps = get_stats(player_games.filter(winner__in=ranked_players,loser__in=ranked_players),[player])
     _,_,_,_,average_stats = get_stats(Game.objects.exclude(advantage=''),Player.objects.all())
     finisher_stats = [(finisher_title,get_finisher_stat(finisher,player)) for finisher,finisher_title in Game.finisher_choices_tuples if finisher!='']
+    rfinisher_stats = [(finisher_title,get_finisher_stat(finisher,player,True)) for finisher,finisher_title in Game.finisher_choices_tuples if finisher!='']
     context = {'player_user':user,'player' : player,'recent_games':recent_games,
     'win_totals':wt,'lose_totals':lt,'win_percentages':wp,'lose_percentages':lp,'player_stats':ps,
     'rwin_totals':rwt,'rlose_totals':rlt,'rwin_percentages':rwp,'rlose_percentages':rlp,'rplayer_stats':rps,
-    'average_stats':average_stats, 'finisher_stats':finisher_stats}
+    'average_stats':average_stats, 'finisher_stats':finisher_stats,'rfinisher_stats':rfinisher_stats}
     return render(request, 'bump/profile.html', context)
   
 def player_info(request):
